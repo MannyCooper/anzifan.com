c9f0ce97d4dea12dcc9c4cd31965ee9c51cd9c21
MannyCooper <an1998821@gmail.com>
2022-06-11 01:32:26 -0700

    Update ContentLayout.js

--- components/layout/ContentLayout.js
+++ components/layout/ContentLayout.js
@@ -11,7 +11,7 @@ export default function ContentLayout({ children }) {
 export function CoverLayout({ children }) {
     return (
         <>
-            <div className="md:w-screen-md lg:w-screen-lg mx-auto my-6 md:px-1 lg:px-15">
+            <div className="md:w-screen-md lg:w-screen-lg mx-auto my-12 md:px-1 lg:px-15">
                 <div className="relative h-56.25vw md:h-100 lg:h-140 overflow-hidden md:rounded-2xl z-0">
                 {children}
                 </div>
