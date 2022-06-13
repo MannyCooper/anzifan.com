33ea9aac3b10076067903bea663878ccbf54a179
MannyCooper <an1998821@gmail.com>
2022-06-12 23:59:03 -0700

    Update PostMore.tsx

--- components/PostMore.tsx
+++ components/PostMore.tsx
@@ -4,6 +4,7 @@ import { Post } from "../lib/types";
 import Link from "next/link";
 import { Colors } from "../lib/colors";
 import Image from "next/image";
+import ThemedImage from "./ThemedImage";
 
 const PostMore: FC<{ posts: Post[] }> = ({ posts }) => {
     return (
@@ -13,17 +14,15 @@ const PostMore: FC<{ posts: Post[] }> = ({ posts }) => {
             </h1>
             <ul className="flex flex-row flex-wrap items-stretch lg:-mr-16 mt-8 more-list">
                 {posts.map((post, index) =>
-                    <li  data-aos="fade-up" className={`lg:mr-16 mb-6 not-last:lg:mb-8 w-full lg:w-114 group lg:odd:after:w-[calc(100%_+_64px)]`} key={post.id} after={`not-last:content-0 flex-shrink-0 flex-grow-0 text-transparent select-none h-[1px] block bg-true-gray-200 mt-6 lg:mt-8 dark:bg-true-gray-700`}>
+                    <li data-aos="fade-up" className={`lg:mr-16 mb-6 not-last:lg:mb-8 w-full lg:w-114 group lg:odd:after:w-[calc(100%_+_64px)]`} key={post.id} after={`not-last:content-0 flex-shrink-0 flex-grow-0 text-transparent select-none h-[1px] block bg-true-gray-200 mt-6 lg:mt-8 dark:bg-true-gray-700`}>
                         <Link href="/post/[slug]" as={`/post/${post.slug}`} >
                             <a className="flex flex-row items-center">
                                 <div className="w-26 h-26 md:(w-33 h-33) rounded-2xl overflow-hidden shrink-0">
                                     <div className={`relative duration-500 ease-in-out filter group-hover:brightness-90 transition w-full h-full rounded-2xl overflow-hidden transform rotate-0`}>
-                                        <Image src={post.cover.light} layout="fill" objectFit="cover" alt={post.title}
-                                            // onLoadingComplete={handleLoad}
-                                            placeholder="blur"
-                                            blurDataURL={post.cover.blurLight}
+                                        <ThemedImage post={post}
                                             className="transition-all duration-500 ease-in-out opacity-100 mobile-hover:group-hover:scale-105
-                                                group-hover:rotate-0 group-hover:active:scale-105 group-hover:opacity-90 transform-gpu rounded-2xl overflow-hidden" />
+                                        group-hover:rotate-0 group-hover:active:scale-105 group-hover:opacity-90 transform-gpu rounded-2xl overflow-hidden"
+                                        />
                                     </div>
                                 </div>
                                 <div className="pl-4 md:pl-6 basis-0 flex-shrink-0 flex-grow">
@@ -43,7 +42,7 @@ const PostMore: FC<{ posts: Post[] }> = ({ posts }) => {
                     </li>
                 )}
             </ul>
-            <div  data-aos="fade-up" className="text-center">
+            <div data-aos="fade-up" className="text-center">
                 <Link href={"/archive"}>
                     <a>
                         <p className="text-center border-2 border-black inline-block py-2 px-5 rounded-full hover:(bg-black  text-white) transition ease-in-out duration-400 mt-6 lg:mt-10" dark="hover:(bg-white text-black) border-white">View Archive</p>
