5d2d7ac0a59f29649b61bf77e10f0f891004d57f
异次元de机智君 <an1998821@gmail.com>
2022-06-30 04:52:32 -0700

    Support PWA

--- next.config.js
+++ next.config.js
@@ -1,18 +1,26 @@
 /** @type {import('next').NextConfig} */
 const WindiCSSWebpackPlugin = require('windicss-webpack-plugin')
 const { withPlaiceholder } = require("@plaiceholder/next");
+const withPWA = require("next-pwa");
 
-module.exports = withPlaiceholder({
-  reactStrictMode: true,
-  webpack(config) {
-    config.plugins.push(new WindiCSSWebpackPlugin)
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"]
-    })
-    return config
-  },
-  images: {
-    domains: ['static.anzifan.com','cdn.sspai.com','cdn.dribbble.com', 'image.freepik.com','avatars.githubusercontent.com','cdn.jsdelivr.net','image.cugxuan.cn','blog-static.mikuchan.top','amazonaws.com'],
-  },
-})
+module.exports = withPWA(
+  withPlaiceholder({
+    reactStrictMode: true,
+    webpack(config) {
+      config.plugins.push(new WindiCSSWebpackPlugin)
+      config.module.rules.push({
+        test: /\.svg$/,
+        use: ["@svgr/webpack"]
+      })
+      return config
+    },
+    images: {
+      domains: ['static.anzifan.com', 'cdn.sspai.com', 'cdn.dribbble.com', 'image.freepik.com', 'avatars.githubusercontent.com', 'cdn.jsdelivr.net', 'image.cugxuan.cn', 'blog-static.mikuchan.top', 'amazonaws.com'],
+    },
+    pwa: {
+      dest: "public",
+      register: true,
+      skipWaiting: true,
+      disable: process.env.NODE_ENV === "development",
+    },
+  }))
