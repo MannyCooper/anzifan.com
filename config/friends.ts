a60f3ae4a747faeeb5936407c1cf5c79883de79b
异次元de机智君 <an1998821@gmail.com>
2022-09-26 18:28:24 -0700

    Update friends.ts

--- config/friends.ts
+++ config/friends.ts
@@ -1,53 +1,53 @@
 export type FriendType = {
-    url: string,
-    name: string,
-    img: string
+  url: string
+  name: string
+  img: string
 }
 
-export const friends : FriendType[] = [
-    {
-        url: 'https://www.ruitz.com/',
-        name: 'rtz的博客',
-        img: 'https://avatars.githubusercontent.com/u/25608822?v=4'
-    },
-    {
-        url: 'https://shiraha.cn/',
-        name: '白羽七海的博客',
-        img: 'https://cdn.jsdelivr.net/gh/45921/cdn-images@main/me/avatar.jpg'
-    },
-    {
-        url: 'https://blog.cugxuan.cn',
-        name: '泫言',
-        img: 'https://image.cugxuan.cn/avater.jpg'
-    },
-    {
-        url: 'https://www.mikuchan.top',
-        name: '团子的航海日记',
-        img: 'https://blog-static.mikuchan.top/avatar.jpg?imageMogr2/thumbnail/!20p/format/webp/blur/1x0/quality/70/w/256/h/256'
-    },
-    {
-        url: 'https://www.ranmc.cn/',
-        name: 'Ranica',
-        img: 'https://avatars.githubusercontent.com/u/69817107'
-    },
-    {
-        url: 'https://blog.ccknbc.cc',
-        name: 'CC康纳百川',
-        img: 'https://cdn.jsdelivr.net/gh/ccknbc-backup/cdn@latest/logo/ccknbc.png'
-    },
-    {
-        url: 'https://blog.zhheo.com/',
-        name: '张洪Heo',
-        img: 'https://img.zhheo.com/i/2022/08/19/62ff32fa28da1.png'
-    },
-    {
-        url: 'https://aohuiliu.fun',
-        name: 'Aohui',
-        img: 'https://www.aohuiliu.fun/images/avatar.jpeg'
-    },
-    {
-        url: 'https://rxhsk.xicp.fun',
-        name: 'Hello',
-        img: 'https://rxhsk.xicp.fun/img/avatar.png'
-    }
+export const friends: FriendType[] = [
+  {
+    url: 'https://www.ruitz.com/',
+    name: 'rtz的博客',
+    img: 'https://avatars.githubusercontent.com/u/25608822?v=4',
+  },
+  {
+    url: 'https://shiraha.cn/',
+    name: '白羽七海的博客',
+    img: 'https://cdn.jsdelivr.net/gh/45921/cdn-images@main/me/avatar.jpg',
+  },
+  {
+    url: 'https://blog.cugxuan.cn',
+    name: '泫言',
+    img: 'https://image.cugxuan.cn/avater.jpg',
+  },
+  {
+    url: 'https://www.mikuchan.top',
+    name: '团子的航海日记',
+    img: 'https://blog-static.mikuchan.top/avatar.jpg?imageMogr2/thumbnail/!20p/format/webp/blur/1x0/quality/70/w/256/h/256',
+  },
+  {
+    url: 'https://www.ranmc.cc',
+    name: 'Ranica',
+    img: 'https://avatars.githubusercontent.com/u/69817107',
+  },
+  {
+    url: 'https://blog.ccknbc.cc',
+    name: 'CC康纳百川',
+    img: 'https://cdn.jsdelivr.net/gh/ccknbc-backup/cdn@latest/logo/ccknbc.png',
+  },
+  {
+    url: 'https://blog.zhheo.com/',
+    name: '张洪Heo',
+    img: 'https://img.zhheo.com/i/2022/08/19/62ff32fa28da1.png',
+  },
+  {
+    url: 'https://aohuiliu.fun',
+    name: 'Aohui',
+    img: 'https://www.aohuiliu.fun/images/avatar.jpeg',
+  },
+  {
+    url: 'https://rxhsk.xicp.fun',
+    name: 'Hello',
+    img: 'https://rxhsk.xicp.fun/img/avatar.png',
+  },
 ]
