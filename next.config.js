cf216209c94ca387fb25a318266eea056cd65a40
异次元de机智君 <an1998821@gmail.com>
2023-12-03 06:05:35 +0800

    Fix withPWA

--- next.config.js
+++ next.config.js
@@ -8,29 +8,30 @@ const withPWA = require('next-pwa')({
     skipWaiting: true,
     disable: process.env.NODE_ENV === "development",
 })
+const nextConfig = {
+    reactStrictMode: true,
+    webpack(config) {
+        config.module.rules.push({
+            test: /\.svg$/,
+            use: ["@svgr/webpack"]
+        })
+        return config
+    },
+    images: {
+        domains: ['static.anzifan.com', 'cdn.sspai.com', 'cdn.dribbble.com', 'image.freepik.com', 'avatars.githubusercontent.com', 'cdn.jsdelivr.net', 'images.unsplash.com'],
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: '*.amazonaws.com',
+            },
+        ],
+    },
+}
 
 
 module.exports =
     withPWA(
         withPlaiceholder(
-            {
-                reactStrictMode: true,
-                webpack(config) {
-                    config.module.rules.push({
-                        test: /\.svg$/,
-                        use: ["@svgr/webpack"]
-                    })
-                    return config
-                },
-                images: {
-                    domains: ['static.anzifan.com', 'cdn.sspai.com', 'cdn.dribbble.com', 'image.freepik.com', 'avatars.githubusercontent.com', 'cdn.jsdelivr.net', 'images.unsplash.com'],
-                    remotePatterns: [
-                        {
-                            protocol: 'https',
-                            hostname: '*.amazonaws.com',
-                        },
-                    ],
-                },
-            }
+            nextConfig
         )
     );
